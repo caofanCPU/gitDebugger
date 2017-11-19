65205f4b96b5a421442c746da832b1f25776fbc2
CaoFan <2804238230@qq.com>
2017-11-20 00:58:48 +0800

    error encoding ocured: git extention was the source of problem

--- src/java/GitChineseCodingErrorAnalysis.java
+++ src/java/GitChineseCodingErrorAnalysis.java
@@ -12,23 +12,14 @@ public class GitChineseCodingErrorAnalysis
      */
     
     /**
-     * third: create chinese words example '你好，是,态态�
+     * third: create chinese words example '你好，是,态态态'
      */
     // first commit code: e8fbfecdcc80f1857069b5fa0fefbad11352fa40
     
     
     /**
-     * forth: add key words: "�
+     * forth: add key words: "态" HHHH
      */
-    // �
+    // 态
     
-    // GGGGGGGGG
-    // second commit code: 9eaa7dbb04acec084b312664928773f852783290
-    
-	// 6684进口发动机发动机
-	
-    // 态个JJJJJJ HHHHHH
-    
-    // ���
-
 }
