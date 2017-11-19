e8fbfecdcc80f1857069b5fa0fefbad11352fa40
CaoFan <2804238230@qq.com>
2017-11-19 23:27:33 +0800

    Initial commit By created a project about git chinese encoding error

---
+++ src/java/GitChineseCodingErrorAnalysis.java
@@ -0,0 +1,18 @@
+/**
+ * Created by CY_XYZ on 2017/11/19.
+ */
+public class GitChineseCodingErrorAnalysis
+{
+    /**
+     * first: create a maven project by maven
+     */
+    
+    /**
+     * second: share project to gitHub
+     */
+    
+    /**
+     * third: create chinese words example '你好，是,态态态'
+     */
+
+}
