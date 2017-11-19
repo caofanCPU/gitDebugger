7f14af6f2339ab4580ca3bc41cd7d8c4385ef432
CaoFan <2804238230@qq.com>
2017-11-19 23:37:16 +0800

    add some info

--- src/java/GitChineseCodingErrorAnalysis.java
+++ src/java/GitChineseCodingErrorAnalysis.java
@@ -14,10 +14,13 @@ public class GitChineseCodingErrorAnalysis
     /**
      * third: create chinese words example '你好，是,态态态'
      */
+    // first commit code: e8fbfecdcc80f1857069b5fa0fefbad11352fa40
+    
     
     /**
      * forth: add key words: "态"
      */
     // 态
+    // second commit code: 9eaa7dbb04acec084b312664928773f852783290
 
 }
